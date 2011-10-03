9922569b678d39a35d1ec8c859d45789051b7ffd
Nicolas <nicolas.papon@webflows.fr>
2011-10-03 15:20:32 +0200

    card menu looks ok

--- card_menu/card_menu.js
+++ card_menu/card_menu.js
@@ -1,4 +1,5 @@
-steal('mxui/nav/menu')
+steal('steal/less')
+.then('mxui/nav/menu', 'jquery/dom/cur_styles', 'itkin/card_menu/card_menu.less')
 .then(
   'jquery-ui/ui/jquery.ui.core.js',
   'jquery-ui/ui/jquery.ui.widget.js')
@@ -16,6 +17,8 @@ steal('mxui/nav/menu')
   },{
     init: function(){
       this._super.apply(this, arguments)
+      this.element.children(this.options.child_selector+':first').addClass('ui-corner-top')
+      this.element.children(this.options.child_selector+':last').addClass('ui-corner-bottom')
     },
     "hide" : function(el, ev){
       var self = this;
@@ -40,12 +43,18 @@ steal('mxui/nav/menu')
       var uls = this.element.find('ul')
       var opener = $('<button class="opener" type="button">options</button>').button({text: false, icons: {primary: 'ui-icon-cancel'}})
 
+      var buttonsetWidth = 0
       this.element.children('a').wrapAll('<div class="toolbar"></div>').parent().append(opener)
         .buttonset()
+        .children()
+        .each(function(){
+          buttonsetWidth += $(this).width()
+        })
 
       var menu = this.element.children('ul')
+//        .width(buttonsetWidth  -5) // correction de 5px
         .itkin_ui_top_right_menu()
-        .mxui_layout_positionable({my: 'right top', at: 'right bottom', offset: "-3px 0", of:opener})
+        .mxui_layout_positionable({my: 'right top', at: 'right bottom', offset: "-3px 0", of:opener}) // correction de 3 px
 
       var elts = $.merge(uls,opener)
 
